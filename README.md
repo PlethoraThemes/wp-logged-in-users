03e0c6d7527defd7e2c417f642c50b256db7b438
Kostas X <kostas@creativecollective.gr>
2014-05-22 16:41:05 +0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-wp-logged-in-users
-==================
+Wordpress Logged in Users
+=========================
 
-Show logged in user on admin bar
+Show logged in users on admin bar
+
+## Installation
+
+### Upload
+
+1. Download the latest tagged archive (choose the "zip" option).
+2. Go to the __Plugins -> Add New__ screen and click the __Upload__ tab.
+3. Upload the zipped archive directly.
+4. Go to the Plugins screen and click __Activate__.
+
+### Manual
+
+1. Download the latest tagged archive (choose the "zip" option).
+2. Unzip the archive.
+3. Copy the folder to your `/wp-content/plugins/` directory.
+4. Go to the Plugins screen and click __Activate__.
+
+Check out the Codex for more information about [installing plugins manually](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+
+### Git
+
+Using git, browse to your `/wp-content/plugins/` directory and clone this repository:
+
+`git clone git@github.com:afragen/github-updater.git`
+
+Then go to your Plugins screen and click __Activate__.
---
+++ wp-logged-in-users.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Plugin Name: Logged In Users
+ * Plugin URI: https://github.com/PlethoraThemes/wp-logged-in-users
+ * Description: Display logged in users on the admin bar
+ * Version: 0.1.0
+ * Author: kostasx
+ * Author URI: https://github.com/kostasx
+ * License: GPL2
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) { die; }
+
+class PlethoraLoggedInUsers {
+
+	protected static $instance = null;
+
+	function __construct(){
+
+		add_action( 'admin_bar_menu', array( $this, 'plethora_admin_bar_add' ), 10    );
+		add_action( 'wp_login', 	  array( $this, 'plethora_wp_login', 	  10, 2 ) );
+		add_action( 'wp_logout', 	  array( $this, 'plethora_wp_logout' 	        ) );
+
+	}
+
+	public static function get_instance(){
+
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+
+	}
+
+	function plethora_helpers_getusers( $type = 'user_login' ){
+
+		$user_list = get_users();
+		$result    = array();
+
+		foreach( $user_list as $user ) $result[] = $user->$type; 
+		return $result;
+
+	}
+
+	public function plethora_admin_bar_add(){
+
+		global $wp_admin_bar; 
+
+		$presence  = get_option( 'plethora_users_online' );
+		$user_list = $this->plethora_helpers_getusers();
+		$title	   = '';	
+
+		foreach ( $user_list as $user  ) {
+
+			if ( $presence[$user] === 'online' ){
+
+				$title .= "<span style='color:green;'>&hearts;</span> " . $user . " | ";
+
+			} else {
+
+				$title .= "<span style='color:red;'>&hearts;</span> " . $user . " | ";
+
+			}
+
+		}
+
+		$wp_admin_bar->add_menu( array( 
+			'id' => 'plethora_liu_admin_bar', 
+	        'parent'=> 'top-secondary',
+			'title' => $title
+			)
+		);
+
+	}
+
+	function plethora_wp_login( $user_login, $user ){
+
+		global $current_user;
+	    get_currentuserinfo();
+
+		$users_online = get_option( 'plethora_users_online' );
+
+		if ( is_array( $users_online ) && 1 !== empty( $users_online) ){
+
+			$users_online[$user_login] = 'online';
+
+		} else {
+
+			$users_online = array( $user_login => 'online' );
+
+		}
+
+		update_option( 'plethora_users_online', $users_online );
+
+	}
+
+	function plethora_wp_logout(){
+
+		$user_login = wp_get_current_user()->user_login;
+
+		$users_online = get_option( 'plethora_users_online' );
+
+		if ( is_array( $users_online ) && 1 !== empty( $users_online) ){
+
+			$users_online[$user_login] = 'offline';
+
+		} else {
+
+			$users_online = array( $user_login => 'offline' );
+
+		}
+
+		update_option( 'plethora_users_online', $users_online );
+
+	}
+
+}
+
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+
+	add_action( 'plugins_loaded', array( 'PlethoraLoggedInUsers', 'get_instance' ) );
+
+}
